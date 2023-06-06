fd5203147506c6de5871526a44200a294a949120
Gencay Turgut <gencayturgut2@gmail.com>
2023-06-06 19:29:14 +0300

    a

--- ISCES/src/main/java/com/ISCES/IscesApplication.java
+++ ISCES/src/main/java/com/ISCES/IscesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IscesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IscesApplication.class, args);
+		System.out.println("selam");
 	}
 
 }
